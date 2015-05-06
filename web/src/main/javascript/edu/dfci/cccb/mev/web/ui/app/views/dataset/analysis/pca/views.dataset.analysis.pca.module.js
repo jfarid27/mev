80b536802478ce12c41ecf68bdaaaaf10a808ad8
J. Farid <jrab227@gmail.com>
2015-05-06 14:48:33 -0400

    Fixed errors

--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/dataset/analysis/pca/views.dataset.analysis.pca.module.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/dataset/analysis/pca/views.dataset.analysis.pca.module.js
@@ -2,7 +2,7 @@ define(['ng', 'lodash'], function(ng, _){
 
     var module = ng.module("mui.views.dataset.analysis.pca", [])
 
-    module.config(["$stateProvider", "$urlRouterProvider", function($sP, $urlRP){})
+    module.config(["$stateProvider", "$urlRouterProvider", function($sP, $urlRP){ }])
 
     module.controller('PcaVM', ['$scope', '$state', '$stateParams', 'project', 'analysis', 
     function($scope, $state, $stateParams, project, analysis){
--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/dataset/analysis/views.dataset.analysis.module.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/dataset/analysis/views.dataset.analysis.module.js
@@ -7,7 +7,7 @@ define(["ng", "lodash",
         "./limma/views.dataset.analysis.limma.module",
         "./deseq/views.dataset.analysis.deseq.module",
         "./nmf/views.dataset.analysis.nmf.module",
-        "./nmf/views.dataset.analysis.pca.module",
+        "./pca/views.dataset.analysis.pca.module",
         "./survival/views.dataset.analysis.survival.module"], function(ng, _){
 	var module = ng.module("mui.views.dataset.analysis", ["mui.widgets.analysis", 
 	                                                      "mui.views.dataset.analysis.hcl", 
